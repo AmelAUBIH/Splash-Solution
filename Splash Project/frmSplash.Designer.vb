960e6ca67be24a90219ebafd30c1410f42993aab
AmelAUBIH <alsc1222@aubih.edu>
2015-11-01 13:12:26 +0100

    Splash Screen Application
    Application from first presentation

---
+++ Splash Project/frmSplash.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSplash
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSplash))
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.tmrExit = New System.Windows.Forms.Timer(Me.components)
+        Me.lblRed = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblMsg
+        '
+        Me.lblMsg.AutoSize = True
+        Me.lblMsg.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMsg.Location = New System.Drawing.Point(395, 192)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(208, 24)
+        Me.lblMsg.TabIndex = 0
+        Me.lblMsg.Text = "A relaxing place to stay!"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(395, 147)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(133, 24)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Red Tree Inn"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(354, 395)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'tmrExit
+        '
+        Me.tmrExit.Enabled = True
+        Me.tmrExit.Interval = 6000
+        '
+        'lblRed
+        '
+        Me.lblRed.AutoSize = True
+        Me.lblRed.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRed.ForeColor = System.Drawing.Color.Red
+        Me.lblRed.Location = New System.Drawing.Point(570, 12)
+        Me.lblRed.Name = "lblRed"
+        Me.lblRed.Size = New System.Drawing.Size(80, 42)
+        Me.lblRed.TabIndex = 3
+        Me.lblRed.Text = "RTI"
+        '
+        'frmSplash
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(662, 429)
+        Me.Controls.Add(Me.lblRed)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblMsg)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmSplash"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Red Tree Inn"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblMsg As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents tmrExit As Timer
+    Friend WithEvents lblRed As Label
+End Class
---
+++ Splash Project/frmSplash.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmSplash
+    Private Sub frmSplash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tmrExit.Start()
+    End Sub
+
+    Private Sub tmrExit_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
+        Me.Close()
+    End Sub
+End Class
